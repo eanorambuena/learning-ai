d577a2ae1ac761d9184a8ce6550d6ba46b0cb475
Emmanuel Norambuena <eanorambuena@uc.cl>
2026-05-25 19:27:34 -0400

    feat: create nlp_lib package with Word2VecLoader, use in nb 23

---
+++ notebooks/nlp/nlp_lib/__init__.py
@@ -0,0 +1,30 @@
+import numpy as np
+from tensorflow.keras import layers
+
+
+class Word2VecLoader:
+    def __init__(self, path='../myWord2Vec/v2/'):
+        target_embeddings = np.load(path + 'target_embeddings.npy')
+        context_embeddings = np.load(path + 'context_embeddings.npy')
+        text_vocab = np.load(path + 'text_vocab.npy', allow_pickle=True).item()
+        self.target_embeddings = target_embeddings
+        self.context_embeddings = context_embeddings
+        self.final_embeddings = (target_embeddings + context_embeddings) / 2
+        self.text_vocab = text_vocab
+        self.idx_to_word = {idx: word for word, idx in text_vocab.items()}
+        self.embedding_dim = target_embeddings.shape[1]
+        self.embedding_layer = layers.Embedding(
+            input_dim=target_embeddings.shape[0],
+            output_dim=target_embeddings.shape[1],
+            weights=[target_embeddings],
+            trainable=True,
+            name='trainable_pretrained_embedding'
+        )
+        print('Embeddings cargados:', target_embeddings.shape, context_embeddings.shape)
+        print('Vocabulario cargado:', len(text_vocab))
+
+    def encode(self, words):
+        return [self.text_vocab[w] for w in words if w in self.text_vocab]
+
+    def decode(self, token_id):
+        return self.idx_to_word.get(int(token_id), '<unk>')
